decadf7fde64a5603ac88f88a923a3de19333fef
Matvey Kukharchuk <bull3003@gmail.com>
2021-04-04 21:57:38 +0300

    stack

---
+++ src/main/java/Stack.java
@@ -0,0 +1,60 @@
+public class Stack<T> {
+    public Node<T> head;
+    public Node<T> tail;
+    public int count;
+
+    public Stack() {
+        this.head = null;
+        this.tail = null;
+        this.count = 0;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public T pop() {
+        if (count == 0)
+            return null;
+        Node<T> valNode = this.head;
+        if (count == 1) {
+            this.head = null;
+            this.tail = null;
+        }
+        T val = valNode.getVal();
+        this.head = valNode.nextNode;
+        count--;
+        return val;
+    }
+
+    public void push(T val) {
+        Node<T> node = new Node<>(val);
+        if (count == 0)
+            this.tail = node;
+        node.nextNode = this.head;
+        this.head = node;
+        count++;
+    }
+
+    public T peek() {
+        return head == null ? null : head.getVal();
+    }
+
+    static class Node<T> {
+        public Node<T> nextNode;
+        public T val;
+
+        public Node(T element) {
+            this.nextNode = null;
+            this.val = element;
+        }
+
+        public void setNextNode(Node<T> nextNode) {
+            this.nextNode = nextNode;
+        }
+
+        public T getVal() {
+            return val;
+        }
+    }
+}
---
+++ src/test/java/StackTest.java
@@ -0,0 +1,47 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StackTest {
+
+    @Test
+    public void testCreateAndPush() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        Assert.assertEquals(stack.size(), 3);
+    }
+
+    @Test
+    public void testPop() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Assert.assertEquals(stack.size(), 3);
+
+        Assert.assertEquals((int) stack.pop(), 3);
+        Assert.assertEquals((int) stack.pop(), 2);
+        Assert.assertEquals((int) stack.pop(), 1);
+        Assert.assertNull(stack.pop());
+
+    }
+
+    @Test
+    public void testPeek() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        Assert.assertEquals(stack.size(), 3);
+
+        Assert.assertEquals((int) stack.peek(), 3);
+        Assert.assertEquals((int) stack.peek(), 3);
+        Assert.assertEquals((int) stack.pop(), 3);
+        Assert.assertEquals((int) stack.pop(), 2);
+        Assert.assertEquals((int) stack.peek(), 1);
+        Assert.assertEquals((int) stack.pop(), 1);
+        Assert.assertNull(stack.peek());
+    }
+}
